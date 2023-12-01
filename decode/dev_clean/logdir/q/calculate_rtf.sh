cf30d9646b1fc66a3a8b5f2de1660a27c4aa76c1
jdannem6 <jdannem6@kent.edu>
2023-11-30 22:10:48 -0500

    Added ASR model data files

---
+++ decode/dev_clean/logdir/q/calculate_rtf.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+cd /ocean/projects/cis230078p/dannemil/espnet_tutorial/espnet/egs2/librispeech_100/asr1
+. ./path.sh
+( echo '#' Running on `hostname`
+  echo '#' Started at `date`
+  set | grep SLURM | while read line; do echo "# $line"; done
+  echo -n '# '; cat <<EOF
+pyscripts/utils/calculate_rtf.py --log-dir exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/dev_clean/logdir --log-name asr_inference --input-shift 0.0625 --start-times-marker "speech length" --end-times-marker "best hypo" --inf-num 1 
+EOF
+) >exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/dev_clean/logdir/calculate_rtf.log
+if [ "$CUDA_VISIBLE_DEVICES" == "NoDevFiles" ]; then
+  ( echo CUDA_VISIBLE_DEVICES set to NoDevFiles, unsetting it... 
+  )>>exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/dev_clean/logdir/calculate_rtf.log
+  unset CUDA_VISIBLE_DEVICES
+fi
+time1=`date +"%s"`
+ ( pyscripts/utils/calculate_rtf.py --log-dir exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/dev_clean/logdir --log-name asr_inference --input-shift 0.0625 --start-times-marker "speech length" --end-times-marker "best hypo" --inf-num 1  ) &>>exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/dev_clean/logdir/calculate_rtf.log
+ret=$?
+sync || true
+time2=`date +"%s"`
+echo '#' Accounting: begin_time=$time1 >>exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/dev_clean/logdir/calculate_rtf.log
+echo '#' Accounting: end_time=$time2 >>exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/dev_clean/logdir/calculate_rtf.log
+echo '#' Accounting: time=$(($time2-$time1)) threads=1 >>exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/dev_clean/logdir/calculate_rtf.log
+echo '#' Finished at `date` with status $ret >>exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/dev_clean/logdir/calculate_rtf.log
+[ $ret -eq 137 ] && exit 100;
+touch exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/dev_clean/logdir/q/done.2531819.$SLURM_ARRAY_TASK_ID
+exit $[$ret ? 1 : 0]
+## submitted with:
+# sbatch --export=PATH  --time 48:00:00 -p RM-shared  --open-mode=append -e exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/dev_clean/logdir/q/calculate_rtf.log -o exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/dev_clean/logdir/q/calculate_rtf.log --array 1-1 /ocean/projects/cis230078p/dannemil/espnet_tutorial/espnet/egs2/librispeech_100/asr1/exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/dev_clean/logdir/q/calculate_rtf.sh >>exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/dev_clean/logdir/q/calculate_rtf.log 2>&1
---
+++ decode/test_clean/logdir/q/asr_inference.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+cd /ocean/projects/cis230078p/dannemil/espnet_tutorial/espnet/egs2/librispeech_100/asr1
+. ./path.sh
+( echo '#' Running on `hostname`
+  echo '#' Started at `date`
+  set | grep SLURM | while read line; do echo "# $line"; done
+  echo -n '# '; cat <<EOF
+python3 -m espnet2.bin.asr_inference --batch_size 1 --ngpu 1 --data_path_and_name_and_type dump/raw/test_clean/wav.scp,speech,kaldi_ark --key_file exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/test_clean/logdir/keys.${SLURM_ARRAY_TASK_ID}.scp --asr_train_config exp/asr_train_asr_raw_en_bpe5000_sp/config.yaml --asr_model_file exp/asr_train_asr_raw_en_bpe5000_sp/valid.acc.ave.pth --output_dir exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/test_clean/logdir/output.${SLURM_ARRAY_TASK_ID} --config conf/decode_asr.yaml 
+EOF
+) >exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/test_clean/logdir/asr_inference.$SLURM_ARRAY_TASK_ID.log
+if [ "$CUDA_VISIBLE_DEVICES" == "NoDevFiles" ]; then
+  ( echo CUDA_VISIBLE_DEVICES set to NoDevFiles, unsetting it... 
+  )>>exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/test_clean/logdir/asr_inference.$SLURM_ARRAY_TASK_ID.log
+  unset CUDA_VISIBLE_DEVICES
+fi
+time1=`date +"%s"`
+ ( python3 -m espnet2.bin.asr_inference --batch_size 1 --ngpu 1 --data_path_and_name_and_type dump/raw/test_clean/wav.scp,speech,kaldi_ark --key_file exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/test_clean/logdir/keys.${SLURM_ARRAY_TASK_ID}.scp --asr_train_config exp/asr_train_asr_raw_en_bpe5000_sp/config.yaml --asr_model_file exp/asr_train_asr_raw_en_bpe5000_sp/valid.acc.ave.pth --output_dir exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/test_clean/logdir/output.${SLURM_ARRAY_TASK_ID} --config conf/decode_asr.yaml  ) &>>exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/test_clean/logdir/asr_inference.$SLURM_ARRAY_TASK_ID.log
+ret=$?
+sync || true
+time2=`date +"%s"`
+echo '#' Accounting: begin_time=$time1 >>exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/test_clean/logdir/asr_inference.$SLURM_ARRAY_TASK_ID.log
+echo '#' Accounting: end_time=$time2 >>exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/test_clean/logdir/asr_inference.$SLURM_ARRAY_TASK_ID.log
+echo '#' Accounting: time=$(($time2-$time1)) threads=1 >>exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/test_clean/logdir/asr_inference.$SLURM_ARRAY_TASK_ID.log
+echo '#' Finished at `date` with status $ret >>exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/test_clean/logdir/asr_inference.$SLURM_ARRAY_TASK_ID.log
+[ $ret -eq 137 ] && exit 100;
+touch exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/test_clean/logdir/q/done.1716271.$SLURM_ARRAY_TASK_ID
+exit $[$ret ? 1 : 0]
+## submitted with:
+# sbatch --export=PATH  -p GPU-shared --gres=gpu:1 -c 1 --time 48:00:00  --open-mode=append -e exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/test_clean/logdir/q/asr_inference.log -o exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/test_clean/logdir/q/asr_inference.log --array 1-8 /ocean/projects/cis230078p/dannemil/espnet_tutorial/espnet/egs2/librispeech_100/asr1/exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/test_clean/logdir/q/asr_inference.sh >>exp/asr_train_asr_raw_en_bpe5000_sp/decode_asr_asr_model_valid.acc.ave/test_clean/logdir/q/asr_inference.log 2>&1
