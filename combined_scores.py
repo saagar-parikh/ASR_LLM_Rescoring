f03a6251809cfb74c9acda835386729a281901db
Saagar Parikh <57773264+saagar-parikh@users.noreply.github.com>
2023-12-02 21:23:31 -0500

    bug fixes

--- combined_scores.py
+++ combined_scores.py
@@ -2,6 +2,8 @@ import torch
 import json
 import argparse
 from torch.nn import functional as F
+import numpy as np
+from tqdm import tqdm
 
 
 def rerank(dict_, id, key):
@@ -28,13 +30,13 @@ def rerank(dict_, id, key):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--test_set", type=str, default="test_other")
-    parser.add_argument("--nbest", type=int, default=5)
+    parser.add_argument("--nbest", type=int, default=10)
     parser.add_argument("--lambda_param", type=int, default=0.3)
     args = parser.parse_args()
 
     test_set = args.test_set
     nbest = args.nbest
-    lambda_param = args.lamda_param
+    lambda_param = args.lambda_param
 
     with open("hyp_llm_10_dict_" + test_set + ".json", "r") as f:
         hyp_dict = json.load(f)
@@ -42,25 +44,30 @@ def main():
     final_dict_bert = {}
     gpt2_dict = {}
     bert_dict = {}
-    for utt_id in hyp_dict:
+    for utt_id in tqdm(hyp_dict):
         sentences = hyp_dict[utt_id]["hypotheses"]
         asr_scores = hyp_dict[utt_id]["scores"]
-        gpt2_scores = hyp_dict[utt_id]["gpt2_scores"]
-        bert_scores = hyp_dict[utt_id]["bert_scores"]
+        gpt2_scores = np.asarray(hyp_dict[utt_id]["gpt2_scores"])
+        bert_scores = np.asarray(hyp_dict[utt_id]["bert_scores"])
         ## if want to take nbest sorting will be required
-        asr_scores_softmax = F.softmax(torch.abs(torch.tensor(asr_scores))).tolist()
-        hyp_dict[utt_id]["final_score_gpt2"] = (
-            1 - lambda_param
-        ) * asr_scores_softmax + lambda_param * gpt2_scores
-        hyp_dict[utt_id]["final_score_bert"] = (
-            1 - lambda_param
-        ) * asr_scores_softmax + lambda_param * bert_scores
+        asr_scores_softmax = F.softmax(
+            torch.abs(torch.tensor(asr_scores)), dim=0
+        ).numpy()
+        hyp_dict[utt_id]["asr_scores_softmax"] = asr_scores_softmax.tolist()
+        hyp_dict[utt_id]["final_score_gpt2"] = list(
+            (1 - lambda_param) * asr_scores_softmax + lambda_param * gpt2_scores
+        )
+        hyp_dict[utt_id]["final_score_bert"] = list(
+            (1 - lambda_param) * asr_scores_softmax + lambda_param * bert_scores
+        )
 
         # Key to sort by
 
+        gpt2_dict[utt_id] = {}
         gpt2_dict[utt_id]["hypotheses"] = hyp_dict[utt_id]["hypotheses"]
         gpt2_dict[utt_id]["final_score_gpt2"] = hyp_dict[utt_id]["final_score_gpt2"]
 
+        bert_dict[utt_id] = {}
         bert_dict[utt_id]["hypotheses"] = hyp_dict[utt_id]["hypotheses"]
         bert_dict[utt_id]["final_score_bert"] = hyp_dict[utt_id]["final_score_bert"]
 
@@ -68,21 +75,22 @@ def main():
         bert_dict = rerank(bert_dict, utt_id, "final_score_bert")
         # Print the updated dictionary
 
-        print(
-            "Sentence: {} and Combined Score for gpt2: {}".format(
-                gpt2_dict[utt_id]["hypotheses"][0],
-                gpt2_dict[utt_id]["final_score_gpt2"][0],
-            )
-        )
-        print(
-            "Sentence: {} and Combined Score for bert: {}".format(
-                gpt2_dict[utt_id]["hypotheses"][0],
-                gpt2_dict[utt_id]["final_score_bert"][0],
-            )
-        )
+        # print(
+        #     "Sentence: {} and Combined Score for gpt2: {}".format(
+        #         gpt2_dict[utt_id]["hypotheses"][-1],
+        #         gpt2_dict[utt_id]["final_score_gpt2"][-1],
+        #     )
+        # )
+        # print(
+        #     "Sentence: {} and Combined Score for bert: {}".format(
+        #         bert_dict[utt_id]["hypotheses"][-1],
+        #         bert_dict[utt_id]["final_score_bert"][-1],
+        #     )
+        # )
 
-        final_dict_gpt2[utt_id] = gpt2_dict[utt_id]["hypotheses"][0]
-        final_dict_bert[utt_id] = gpt2_dict[utt_id]["hypotheses"][0]
+        final_dict_gpt2[utt_id] = gpt2_dict[utt_id]["hypotheses"][-1]
+        final_dict_bert[utt_id] = bert_dict[utt_id]["hypotheses"][-1]
+        # break
 
     with open("hyp_comb_" + str(nbest) + "_dict_" + test_set + ".json", "w") as outfile:
         json.dump(hyp_dict, outfile, indent=2)
